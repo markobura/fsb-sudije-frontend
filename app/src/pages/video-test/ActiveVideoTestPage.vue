03871896280e5716ce149176a34ef137baa98d68
Marko Bakic <marko.bakic@kentkart.rs>
2025-01-28 17:03:41 +0100

    asd

--- app/src/pages/video-test/ActiveVideoTestPage.vue
+++ app/src/pages/video-test/ActiveVideoTestPage.vue
@@ -4,93 +4,128 @@
       <q-card-section>
         <BaseHeader icon="quiz" title="VIDEO TEST"></BaseHeader>
       </q-card-section>
-      <q-separator inset/>
-        <q-stepper
-        v-model="step"
-        vertical
-        color="primary"
-        animated
-      >
+      <q-separator inset />
+      <q-stepper v-model="step" vertical color="primary" animated>
         <q-step
           icon="quiz"
-          v-for="(question, index) in questions" :key="question"
+          v-for="(question, index) in questions"
+          :key="question"
           :name="index"
           :done="step > index"
-          :title="index+1 + '. Pitanje'"
+          :title="index + 1 + '. Pitanje'"
         >
-            <div style="width: 100%">
-              <q-item style="display: flex; justify-content: center; margin-bottom: 20px">
-                <video controls style="max-width: 600px;" class="mobile-display">
-                  <source :src="question.video" type="video/mp4"/>
-                </video>
-              </q-item>
-              <q-item v-for="(answers,index2) in question.answers" :key="answers" dense style="margin-bottom: 10px">
-                <q-item-section>
-                  <span>
-                  <strong class="text-primary">{{getAnswerOrder(index2)}}</strong>  {{answers.answer_text}}
-                  </span>
-                </q-item-section>
-                <q-item-section side >
-                  <q-toggle color="green" v-model="answers.is_correct"
-                            @update:model-value="updateAnswers(index,index2)"/>
-                </q-item-section>
-              </q-item>
-            </div>
-            <q-separator inset/>
-            <q-stepper-navigation style="text-align: right">
-              <q-btn v-if="step === questions.length-1" @click="submit" color="green" label="Završi" rounded size='lg'/>
-              <q-btn v-else @click="nextStep(index)" color="primary" label="Sledeće pitanje" rounded/>
-            </q-stepper-navigation>
-
+          <div style="width: 100%" v-if="step === index">
+            <q-item
+              style="
+                display: flex;
+                justify-content: center;
+                margin-bottom: 20px;
+              "
+            >
+              <video controls style="max-width: 600px" class="mobile-display">
+                <source :src="question.video" type="video/mp4" />
+              </video>
+            </q-item>
+            <q-item
+              v-for="(answers, index2) in question.answers"
+              :key="answers"
+              dense
+              style="margin-bottom: 10px"
+            >
+              <q-item-section>
+                <span>
+                  <strong class="text-primary">{{
+                    getAnswerOrder(index2)
+                  }}</strong>
+                  {{ answers.answer_text }}
+                </span>
+              </q-item-section>
+              <q-item-section side>
+                <q-toggle
+                  color="green"
+                  v-model="answers.is_correct"
+                  @update:model-value="updateAnswers(index, index2)"
+                />
+              </q-item-section>
+            </q-item>
+          </div>
+          <q-separator inset />
+          <q-stepper-navigation style="text-align: right">
+            <q-btn
+              v-if="step === questions.length - 1"
+              @click="submit"
+              color="green"
+              label="Završi"
+              rounded
+              size="lg"
+            />
+            <q-btn
+              v-else
+              @click="nextStep(index)"
+              color="primary"
+              label="Sledeće pitanje"
+              rounded
+            />
+          </q-stepper-navigation>
         </q-step>
-        </q-stepper>
+      </q-stepper>
     </q-card>
   </q-page>
 
   <q-page v-else class="row items-center justify-evenly">
-
-    <q-card style="display: flex; height: 200px; max-width: 330px; background-color: #D3D3D3" class="no-padding">
-      <q-card-section style="align-self: center" >
+    <q-card
+      style="
+        display: flex;
+        height: 200px;
+        max-width: 330px;
+        background-color: #d3d3d3;
+      "
+      class="no-padding"
+    >
+      <q-card-section style="align-self: center">
         <q-icon name="warning" color="primary" size="50px"></q-icon>
       </q-card-section>
-      <q-card-section style="align-self: center; display: flex; flex-direction: column">
+      <q-card-section
+        style="align-self: center; display: flex; flex-direction: column"
+      >
         <q-card-section class="text-center text-bold" style="font-size: 16px">
-          Trenutno ne postoji aktivan test!
-          Pokušajte ponovo kasnije.
+          Trenutno ne postoji aktivan test! Pokušajte ponovo kasnije.
         </q-card-section>
-        <q-card-actions style="align-self: center;">
-          <q-btn @click="reloadPage" rounded label="Pokušaj ponovo" color="primary"></q-btn>
+        <q-card-actions style="align-self: center">
+          <q-btn
+            @click="reloadPage"
+            rounded
+            label="Pokušaj ponovo"
+            color="primary"
+          ></q-btn>
         </q-card-actions>
       </q-card-section>
     </q-card>
-
-
   </q-page>
 </template>
 
 <script setup lang="ts">
-import {useQuasar} from "quasar";
-import {useRouter} from "vue-router";
-import {useTheoryAndVideoTestStore} from "stores/theoryAndVideoTestStore";
-import {computed, ref} from "vue";
-import BaseHeader from 'src/components/BaseHeader.vue'
-import useNotificationMessage from "src/composables/notificationMessage";
-import {test} from "vitest";
+import { useQuasar } from 'quasar';
+import { useRouter } from 'vue-router';
+import { useTheoryAndVideoTestStore } from 'stores/theoryAndVideoTestStore';
+import { computed, ref } from 'vue';
+import BaseHeader from 'src/components/BaseHeader.vue';
+import useNotificationMessage from 'src/composables/notificationMessage';
+import { test } from 'vitest';
 
 const $q = useQuasar();
 const router = useRouter();
 
 const videoTestStore = useTheoryAndVideoTestStore();
 
-const activeTestExist = computed(()=>{
+const activeTestExist = computed(() => {
   return videoTestStore.activeVideoTestExist;
-})
+});
 
-
-async function getActiveTest(){
+async function getActiveTest() {
   await videoTestStore.getActiveVideoTestApi();
   questions.value = videoTestStore.getActiveVideoTest.questions;
-  questions.value = shuffleArray(questions.value)
+  questions.value = shuffleArray(questions.value);
 }
 getActiveTest();
 
@@ -101,81 +136,86 @@ function shuffleArray(array: any[]) {
   }
   return array;
 }
-const step = ref(0)
+const step = ref(0);
 let questions = ref([
   {
     video: '',
     order_id: '',
     answers: [
-      {answer_text: '',is_correct: false},
-      {answer_text: '',is_correct: false},
-      {answer_text: '',is_correct: false},
-      {answer_text: '',is_correct: false}
+      { answer_text: '', is_correct: false },
+      { answer_text: '', is_correct: false },
+      { answer_text: '', is_correct: false },
+      { answer_text: '', is_correct: false },
     ],
   },
-])
+]);
 
-function reloadPage(){
+function reloadPage() {
   location.reload();
 }
 
-function getAnswerOrder(index: number){
-  switch (index){
-    case  0 :
-      return 'a) '
-    case  1 :
-      return 'b) '
-    case  2 :
-      return 'c) '
-    case  3 :
-      return 'd) '
-    case  4 :
-      return 'e) '
-    case  5 :
-      return 'f) '
-    case  6 :
-      return 'g) '
+function getAnswerOrder(index: number) {
+  switch (index) {
+    case 0:
+      return 'a) ';
+    case 1:
+      return 'b) ';
+    case 2:
+      return 'c) ';
+    case 3:
+      return 'd) ';
+    case 4:
+      return 'e) ';
+    case 5:
+      return 'f) ';
+    case 6:
+      return 'g) ';
   }
 }
-function updateAnswers(indexAnswer: number,correctAnswer: number){
-  questions.value[indexAnswer].answers.forEach((el,index) => {
-    if(index !== correctAnswer){
-      el.is_correct = false
+function updateAnswers(indexAnswer: number, correctAnswer: number) {
+  questions.value[indexAnswer].answers.forEach((el, index) => {
+    if (index !== correctAnswer) {
+      el.is_correct = false;
     }
-  })
+  });
 }
 
 const showConfirmationDialog = ref(true);
 
-function nextStep(index: number){
-  if(!validationSuccessful(index)){
+function nextStep(index: number) {
+  if (!validationSuccessful(index)) {
     return;
   }
 
-  if(showConfirmationDialog.value) {
+  if (showConfirmationDialog.value) {
     $q.dialog({
       title: 'Naredno pitanje',
-      message: 'Ukoliko odete na naredno pitanje, više se ne možete vratiti na ovo pitanje.',
+      message:
+        'Ukoliko odete na naredno pitanje, više se ne možete vratiti na ovo pitanje.',
       options: {
         type: 'checkbox',
         model: [],
         items: [
-          {label: 'Ne prikazuj ovaj dialog više', value: '1', color: 'secondary'},
-        ]
+          {
+            label: 'Ne prikazuj ovaj dialog više',
+            value: '1',
+            color: 'secondary',
+          },
+        ],
       },
       persistent: true,
       ok: {
         push: true,
-        color: 'positive'
+        color: 'positive',
       },
-      cancel: true
+      cancel: true,
     }).onOk((data) => {
-      if(data.includes('1')){
+      if (data.includes('1')) {
         showConfirmationDialog.value = false;
       }
       step.value++;
-    })
-  }else {
+    });
+  } else {
     step.value++;
   }
 }
@@ -184,37 +224,35 @@ function sortByOrderId(a: any, b: any) {
   return a.order_id - b.order_id;
 }
 
-async function submit(){
-  if(!validationSuccessful(questions.value.length -1)){
+async function submit() {
+  if (!validationSuccessful(questions.value.length - 1)) {
     return;
   }
-  let testAnswers : { answer: string }[] = [];
+  let testAnswers: { answer: string }[] = [];
 
+  questions.value.sort(sortByOrderId);
 
-  questions.value.sort(sortByOrderId)
-
-
-
-  questions.value.forEach(question => {
-    const index = question.answers.findIndex(answer => answer.is_correct);
-    if(index !== -1){
-      testAnswers.push({answer: question.answers[index].answer_text})
+  questions.value.forEach((question) => {
+    const index = question.answers.findIndex((answer) => answer.is_correct);
+    if (index !== -1) {
+      testAnswers.push({ answer: question.answers[index].answer_text });
     }
-  })
+  });
 
   await videoTestStore.submitVideoTest(testAnswers);
   await router.push({
-    name: 'home'
-  })
+    name: 'home',
+  });
   location.reload();
 }
 
-function validationSuccessful(questionIndex: number){
+function validationSuccessful(questionIndex: number) {
+  const answerIsNotSelected = questions.value[questionIndex].answers.every(
+    (answer) => !answer.is_correct
+  );
 
-  const answerIsNotSelected = questions.value[questionIndex].answers.every(answer => !answer.is_correct);
-
-  if(answerIsNotSelected){
-    useNotificationMessage('error','Morate označiti odgovor!');
+  if (answerIsNotSelected) {
+    useNotificationMessage('error', 'Morate označiti odgovor!');
     return false;
   }
 
